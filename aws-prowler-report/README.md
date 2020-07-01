8ffc9a9d5d1ce6ddfbfce5eccc6084e274f2fa85
Keith McCanless <keith@keiths-mbp.lan>
2020-07-01 09:52:21 -0400

    adding tool to create html report from prowler json

---
+++ aws-prowler-report/README.md
@@ -0,0 +1,12 @@
+Run Prowler (https://github.com/toniblyx/prowler)
+```./prowler -g cislevel2 -r region -M json```
+
+Move output to input folder under this project.  DO NOT CHECK IN JSON!
+```node ./index.js node ./index.js -f "./test-data/sgp.json" --out ./output/sgp.html -c Roche ```
+
+
+Install wkhtmltopdf (https://wkhtmltopdf.org/downloads.html)
+Run wkhtmltopdf passing in the .html and the output pdf file as below.  Make
+sure to use the  --print-media-type param.
+
+```wkhtmltopdf report.html --print-media-type cmd.pdf```
---
+++ aws-prowler-report/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const _ = require('lodash');
+const pug = require('pug');
+const moment = require('moment');
+const { program } = require('commander');
+
+
+program.version('0.0.1');
+program.option('-f, --file <file>', 'prowler json ouput',"./input/test-data/prowler-output-sample.json")
+program.option('-o, --out <file>', 'output html', './output/report.html')
+program.option('-c, --customer <name>', 'customer for report', 'ACME')
+program.parse(process.argv);
+let raw = `${program.file}`;
+let out = `${program.out}`;
+let customer = `${program.customer}`;
+//let raw = 'prowler-output-sample.json'
+let file = fs.readFileSync(raw);
+let json = file.toString().trim().split('\n').map(JSON.parse);
+let grouped = _.groupBy(json,"Control ID");
+let audit = [];
+for (a in grouped){
+    let test = grouped[a].reduce((n,o) => {
+        (n.Results || (n.Results = [])).push({"Status": o.Status, "Message": o.Message});
+        n.Profile = o.Profile;
+        n["Account Number"] = o["Account Number"];
+        n["Control ID"] = o["Control ID"];
+        n.Control = o.Control;
+        n.Scored = o.Scored;
+        n.Level = o.Level;
+        n.Region = o.Region;
+        return n;
+    },{});
+    audit.push(test);
+    console.log(audit);
+}
+fs.writeFileSync('report.json',JSON.stringify(audit));
+const compiledFunction = pug.compileFile('./assets/template.pug');
+fs.writeFileSync(out,compiledFunction({'json': audit, 'moment': moment, 'customer' : customer}));
+
+exec('"/path/to/test file/test.sh" arg1 arg2');
